5e9555b65a57bf21ff12174637be17c56a6b864f
silverbackdan <daniel@silverback.is>
2025-06-02 18:03:23 +0100

    Trying to replicate the break

--- demo/server/api/tasks.ts
+++ demo/server/api/tasks.ts
@@ -1,8 +1,13 @@
 import { createError } from 'h3'
 import type { Database } from '~~/types/database.types'
-import { serverSupabaseUser, serverSupabaseClient } from '#supabase/server'
+import {serverSupabaseUser, serverSupabaseClient, serverSupabaseServiceRole} from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
+  const body = await readBody(event)
+  console.log(body)
+  const serviceClient = serverSupabaseServiceRole<Database>(event)
+  await serviceClient.from('someTable').insert([ { key: 'data' } ])
+
   const user = await serverSupabaseUser(event)
   const client = await serverSupabaseClient<Database>(event)
 
